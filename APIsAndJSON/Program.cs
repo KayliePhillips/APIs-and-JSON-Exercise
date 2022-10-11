59450495cde2e73159937d6b30124288efe8dfa8
Kaylie Phillips <kaylie.travel@gmail.com>
2022-10-11 18:19:13 -0500

    Completed Exercise 1

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var client = new HttpClient();
+            var convo = new RonVSKanyeAPI(client);
+
+            //call the method
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Kanye: {convo.KanyeTalking()}");
+                Console.WriteLine();
+                Console.WriteLine($"Ron: {convo.RonTalking()}");
+                Console.WriteLine();
+            }
+            
+
         }
     }
 }
--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,38 @@ using System.Threading.Tasks;
 
 namespace APIsAndJSON
 {
-    internal class RonVSKanyeAPI
+    public class RonVSKanyeAPI
     {
+        private readonly HttpClient _client;
+
+        public RonVSKanyeAPI(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public string KanyeTalking()
+        {
+            var kanyeURL = "https://api.kanye.rest/";
+
+            var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+
+        }
+
+        public string RonTalking()
+        {
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = _client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            return ronQuote;   
+        }
+
     }
 }
+
